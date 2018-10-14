4302b698e711d5f811b178c4cde7c291de9c8d62
drache <drache@dracheportatil>
2018-10-14 11:58:47 -0500

    corregir inserts

--- docker-compose/mysql/createDB.sql
+++ docker-compose/mysql/createDB.sql
@@ -5,7 +5,7 @@ CREATE TABLE course (
   themes varchar(250) DEFAULT NULL,
   CONSTRAINT categorias_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO taller1.course ('name', 'themes') VALUES
+INSERT INTO taller1.course (name, themes) VALUES
 ('Curso microservicios 1', 'Temas curso 1');
-INSERT INTO taller1.course ('name', 'themes') VALUES
+INSERT INTO taller1.course (name, themes) VALUES
 ('Curso microservicios 2', 'Temas curso 2');
