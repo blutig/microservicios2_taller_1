92df3e1cf0d01f0f3fea5e53cc0e49b873cb5834
drache <drache@dracheportatil>
2018-10-14 11:49:32 -0500

    modificar tabla

--- docker-compose/mysql/createDB.sql
+++ docker-compose/mysql/createDB.sql
@@ -1,10 +1,11 @@
 USE taller1;
 CREATE TABLE 'course' (
-  'id_course' int(11) NOT NULL,
-  'name' varchar(250) NOT NULL,
-  'themes' varchar(250) DEFAULT NULL
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(250) NOT NULL,
+  themes varchar(250) DEFAULT NULL,
+  CONSTRAINT categorias_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO 'course' ('id_course', 'name', 'themes') VALUES
+INSERT INTO 'course' ('id', 'name', 'themes') VALUES
 (1,  'Curso microservicios 1', 'Temas curso 1');
-INSERT INTO 'course' ('id_course', 'name', 'themes') VALUES
+INSERT INTO 'course' ('id', 'name', 'themes') VALUES
 (2,  'Curso microservicios 2', 'Temas curso 2');
