eef292598cb692e95c757d5963038798ace58c2e
drache <drache@dracheportatil>
2018-10-15 15:07:18 -0500

    docker-compose

--- docker-compose/mysql/createDB.sql
+++ docker-compose/mysql/createDB.sql
@@ -3,7 +3,7 @@ CREATE TABLE course (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(250) NOT NULL,
   themes varchar(250) DEFAULT NULL,
-  CONSTRAINT categorias_pk PRIMARY KEY (id)
+  CONSTRAINT course_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 INSERT INTO taller1.course (name, themes) VALUES
 ('Curso microservicios 1', 'Temas curso 1');
